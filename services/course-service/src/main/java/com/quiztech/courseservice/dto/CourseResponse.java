ebb24a2bf69a197a625c13afd14f8016afb4f016
Fuji-Vanilli <ijufillivan@gmail.com>
2024-06-22 11:08:03 +0000

    add course-service

---
+++ services/course-service/src/main/java/com/quiztech/courseservice/dto/CourseResponse.java
@@ -0,0 +1,19 @@
+package com.quiztech.courseservice.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CourseResponse {
+    private String id;
+    private String title;
+    private String description;
+    private Date createdDate;
+    private Date lastUpdateDate;
+    private String pathLesson;
+}
